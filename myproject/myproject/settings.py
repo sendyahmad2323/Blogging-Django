07b7085d95a3dddaa15cddc52e23205963ce5564
sendyahmad2323 <sendyahmad415@gmail.com>
2025-06-28 19:24:44 +0800

    update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -114,6 +114,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 # Default auto field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
